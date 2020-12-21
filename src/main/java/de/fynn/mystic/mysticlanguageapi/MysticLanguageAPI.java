feca5a868215658bc66c48f4f5ede3396ce83678
Fynn Schur <fynn.schur@web.de>
2020-12-21 13:50:07 +0100

    Initial commit

---
+++ src/main/java/de/fynn/mystic/mysticlanguageapi/MysticLanguageAPI.java
@@ -0,0 +1,16 @@
+package de.fynn.mystic.mysticlanguageapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MysticLanguageAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        this.getLogger().info("Enabling MysticLanguageAPI");
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
